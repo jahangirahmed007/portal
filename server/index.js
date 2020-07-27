5e662551dba1b65faa7f50b704f48f66a533ab26
Store- <jahangirahmedawan007@gmail.com>
2020-07-27 22:50:39 +0500

    ulock

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use("/portal-fare", routes);
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../build", "index.html"));
 // });
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
