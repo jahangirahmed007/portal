5715b46ea615208ff5b9309ef886e5fb8ac2d0ca
Store- <jahangirahmedawan007@gmail.com>
2020-07-27 20:57:15 +0500

    some other packages changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ dotenv.config();
 const routes = require("./Routes/routes");
 const app = express();
 const path = require("path");
-app.use(express.static(path.join(__dirname, "../build")));
 const passport = require("passport");
 require("./passport/JWT-Strategy")(passport);
 Middleware();
@@ -15,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 
+app.use(express.static(path.join(__dirname, "../build")));
 app.use("/portal-fare", routes);
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../build", "index.html"));
