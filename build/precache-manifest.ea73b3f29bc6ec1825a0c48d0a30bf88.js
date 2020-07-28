37304434a207c65e9b528baf6e3d487052e89f82
Store- <jahangirahmedawan007@gmail.com>
2020-07-28 12:18:02 +0500

    new build version

---
+++ build/precache-manifest.ea73b3f29bc6ec1825a0c48d0a30bf88.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4a5af8ed05361fd951b0fca0ac1a3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d05e0c81aa2492e4e76",
+    "url": "/static/css/0.95044d84.chunk.css"
+  },
+  {
+    "revision": "308b63f0babd87d3c706",
+    "url": "/static/css/3.cc73468a.chunk.css"
+  },
+  {
+    "revision": "b9acd56a46b6f69cb207",
+    "url": "/static/css/4.dff11115.chunk.css"
+  },
+  {
+    "revision": "de241a9b07e916b814f6",
+    "url": "/static/css/5.dff11115.chunk.css"
+  },
+  {
+    "revision": "ab9dd29fb86dba1bf736",
+    "url": "/static/css/main.7a5a0d1e.chunk.css"
+  },
+  {
+    "revision": "7d05e0c81aa2492e4e76",
+    "url": "/static/js/0.7c694fe9.chunk.js"
+  },
+  {
+    "revision": "1dd57bb86f32108fc145",
+    "url": "/static/js/10.0009c934.chunk.js"
+  },
+  {
+    "revision": "308b63f0babd87d3c706",
+    "url": "/static/js/3.28746faa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.28746faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9acd56a46b6f69cb207",
+    "url": "/static/js/4.bcd926f4.chunk.js"
+  },
+  {
+    "revision": "de241a9b07e916b814f6",
+    "url": "/static/js/5.74e41281.chunk.js"
+  },
+  {
+    "revision": "78a830097ebcbd11dd74",
+    "url": "/static/js/6.5f639041.chunk.js"
+  },
+  {
+    "revision": "54342e6f829901710435",
+    "url": "/static/js/7.eaf2bff1.chunk.js"
+  },
+  {
+    "revision": "9dbc161ca53a054edbf5",
+    "url": "/static/js/8.a523d80f.chunk.js"
+  },
+  {
+    "revision": "8b2d4dd204f387e50911",
+    "url": "/static/js/9.7362c5cd.chunk.js"
+  },
+  {
+    "revision": "ab9dd29fb86dba1bf736",
+    "url": "/static/js/main.777db9f0.chunk.js"
+  },
+  {
+    "revision": "e82ed4592eb8021e6b7c",
+    "url": "/static/js/runtime-main.39861549.js"
+  }
+]);
